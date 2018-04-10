60067d7dd34b5f055f9d4e0249fa60c18b38e57a
Edward <krisandjohn42@hotmail.com>
2018-04-10 17:12:46 -0500

    i made this with ArrayList's

--- src/CiyBomb.java
+++ src/CiyBomb.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Random;
 
 
@@ -10,38 +11,34 @@ public class CiyBomb
 	public static void main(String[] args) 
 	{
 		int NumberOfCities = getIntInput("Enter number of cities: ");
-		int[] cityHealth = new int[NumberOfCities];
+		//int[] cityHealth = new int[NumberOfCities];
+		ArrayList<Integer> cityHealth = new ArrayList<Integer>();
 		int maxBlast = getIntInput("Enter bomb damage: ");
 		int maxbombs = getIntInput("Enter number of bomb runs: ");
-		cityHealth[0] = getIntInput("Enter the cities HP (Int): ");
+		cityHealth.add(getIntInput("Enter the cities HP (Int): "));
 
 		for (int i = 1; i < NumberOfCities; i++) 
 		{
-			cityHealth[i] = cityHealth[0];
+			cityHealth.add(cityHealth.get(0));
 		}
-		PrintCityHealth(cityHealth, NumberOfCities);
+		PrintCityHealth(cityHealth);
 		
 		for(int i = 1; i < maxbombs; i++)
 		{
 			for(int i2 = 0; i2 < NumberOfCities; i2++)
 			{
-				cityHealth[i2] = cityHealth[i2] - bomb(maxBlast);
-			}
-			PrintCityHealth(cityHealth, NumberOfCities);
+				//cityHealth[i2] = cityHealth[i2] - bomb(maxBlast);
+				cityHealth.set(i2, cityHealth.get(i2) - bomb(maxBlast));
+			}			PrintCityHealth(cityHealth);
 		}
-		
-		
-	//	int x = (98 + 54 * 63 + 43 * 324 * 21);
-		
-		//System.out.print(x);
 	}
 	
 	
 	
-	public static void PrintCityHealth(int[] cities, int numOfCtys) 
+	public static void PrintCityHealth(ArrayList cities) 
 	{
-		for(int i = 0; i  < numOfCtys; i++)
-			System.out.printf("%9d", cities[i]);
+		for(int i = 0; i  < cities.size(); i++)
+			System.out.printf("%9d", cities.get(i));
 		System.out.println(" ");
 	}
 	
